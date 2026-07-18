b709d973cf2e7559d242c462f726f7714f287a03
hikaru <makinaminami@gmail.com>
2026-07-19 00:28:31 +0900

    コピーライトの西暦を動的に

--- java.js
+++ java.js
@@ -1,13 +1,22 @@
-const body = document.body;
-const ham = document.querySelector(".ham");
-const menu = document.querySelector(".main-navigation");
-ham.addEventListener("click", () => {
-  body.classList.toggle("open");
-});
-menu.addEventListener("click", () => {
-  body.classList.remove("open");
-});
-let focusTrap = document.getElementById("js-focus-trap");
-focusTrap.addEventListener("focus", (e) => {
-  ham.focus();
+document.addEventListener("DOMContentLoaded", function () {
+  // ハンバーガーの開閉とタブ操作の制御
+  const body = document.body;
+  const ham = document.querySelector(".ham");
+  const menu = document.querySelector(".main-navigation");
+  ham.addEventListener("click", () => {
+    body.classList.toggle("open");
+  });
+  menu.addEventListener("click", () => {
+    body.classList.remove("open");
+  });
+  let focusTrap = document.getElementById("js-focus-trap");
+  focusTrap.addEventListener("focus", (e) => {
+    ham.focus();
+  });
+
+  // コピーライトの西暦を動的に出力する
+  const yearEl = document.getElementById("year");
+  if (yearEl) {
+    yearEl.textContent = new Date().getFullYear();
+  }
 });
